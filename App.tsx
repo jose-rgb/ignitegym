0a6dcc356962ba8b300014bb7d2e88c4a76edd44
José Ricardo <josericardosaraiva7@gmail.com>
2024-06-08 10:47:43 -0300

    config: routes

--- App.tsx
+++ App.tsx
@@ -2,9 +2,10 @@ import { StatusBar } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
+import { Routes } from '@routes/index';
+
 import { THEME } from 'src/theme';
 import { Loading } from '@components/Loading';
-import { SignUp } from '@screens/SignUp';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold});
@@ -12,7 +13,7 @@ export default function App() {
   return (
     <NativeBaseProvider theme={THEME}>
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent/>
-      {fontsLoaded ? <SignUp /> :  <Loading />}
+      {fontsLoaded ? <Routes /> :  <Loading />}
     </NativeBaseProvider>
   );
 }

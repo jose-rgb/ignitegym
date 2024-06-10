0f43d85f88d23ba873abc672ff6ac928d58de94d
José Ricardo <josericardosaraiva7@gmail.com>
2024-06-10 16:23:03 -0300

    refactor: bugs design

--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -1,4 +1,4 @@
-import { HStack, Heading, Icon, Text, VStack, Image, Box } from "native-base";
+import { HStack, Heading, Icon, Text, VStack, Image, Box, ScrollView } from "native-base";
 import { TouchableOpacity } from "react-native";
 import { Feather } from '@expo/vector-icons'
 import { useNavigation } from "@react-navigation/native";
@@ -19,7 +19,7 @@ export function Exercise(){
 
     return(
         <VStack flex={1}>
-
+            
             <VStack px={8} bg="gray.600" pt={12}>
                 <TouchableOpacity onPress={hanndleGoBack}>
                     <Icon
@@ -40,38 +40,41 @@ export function Exercise(){
                 </HStack>
             </VStack>
 
-            <VStack p={8}>
-                <Image
-                    w="full"
-                    h={80}
-                    source={{uri:'https://avatars.githubusercontent.com/u/61853617?v=4'}}
-                    alt="Nome do exercício"
-                    mb={3}
-                    resizeMode="cover"
-                    rounded="lg"
-                />
 
-                <Box bg="gray.600" rounded="md" pb={4} px={4}>
-                    <HStack alignItems="center" justifyContent="space-around" mb={6} mt={5}>
-
-                        <HStack>
-                            <SeriesSvg />
-                            <Text color="gray.200" ml="2"> 3 séries </Text>
-                        </HStack>
-
-                        <HStack>
-                            <RepetitionsSvg />
-                            <Text color="gray.200" ml="2"> 12 repetições </Text>
-                        </HStack>
-
-                    </HStack>   
-
-                    <Button  
-                        title="Marcar como realizado"
+            <ScrollView>
+                <VStack p={8}>
+                    <Image
+                        w="full"
+                        h={80}
+                        source={{uri:'https://avatars.githubusercontent.com/u/61853617?v=4'}}
+                        alt="Nome do exercício"
+                        mb={3}
+                        resizeMode="cover"
+                        rounded="lg"
                     />
-                </Box>
 
-            </VStack>
+                    <Box bg="gray.600" rounded="md" pb={4} px={4}>
+                        <HStack alignItems="center" justifyContent="space-around" mb={6} mt={5}>
+
+                            <HStack>
+                                <SeriesSvg />
+                                <Text color="gray.200" ml="2"> 3 séries </Text>
+                            </HStack>
+
+                            <HStack>
+                                <RepetitionsSvg />
+                                <Text color="gray.200" ml="2"> 12 repetições </Text>
+                            </HStack>
+
+                        </HStack>   
+
+                        <Button  
+                            title="Marcar como realizado"
+                        />
+                    </Box>
+
+                </VStack>
+            </ScrollView>
         </VStack>
     );
 }

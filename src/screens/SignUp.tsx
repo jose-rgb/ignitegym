a4645b8039e37b9c712ddcb37d28d5748a71c7d6
José Ricardo <josericardosaraiva7@gmail.com>
2024-06-14 11:43:47 -0300

    feat: configured axios & api errors

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,8 +1,11 @@
 import { useNavigation } from '@react-navigation/native';
-import { VStack, Image, Text, Center, Heading, ScrollView } from 'native-base'; 
+import { VStack, Image, Text, Center, Heading, ScrollView, useToast} from 'native-base'; 
 import { useForm, Controller} from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
+
+import { api } from '@services/api';
+import { AppError } from '@utils/AppError';
 
 import LogoSvg from '@assets/logo.svg';
 import BackgroundImg from '@assets/background.png';
@@ -27,6 +30,8 @@ const signUpSchema = yup.object({
 
 export function SignUp() {
 
+    const toast = useToast();
+
     const { control, handleSubmit, formState: { errors } } = useForm<FormDataProps>({
         resolver: yupResolver(signUpSchema)
     });
@@ -38,18 +43,23 @@ export function SignUp() {
     }
 
     async function handleSignUp({ name, email, password}: FormDataProps) {
-        const response = await fetch('http://192.168.3.6:3333/users', {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({name, email, password})
-        });
-        
-        const data = await response.json();
-        console.log(data);
+        try {
+
+            const response = await api.post('/users', {name, email, password});
+
+        } catch (error) {
+            const isAppError = error instanceof AppError;
+
+            const title = isAppError ? error.message : 'Não foi possível criar a conta. Tente novamente mais tarde';
+
+            toast.show({
+                title,
+                placement: 'top',
+                bgColor: 'red.500'
+            })
+        }
     }
+
     
     return(
         <ScrollView contentContainerStyle={{flexGrow: 1}} showsVerticalScrollIndicator={false}>

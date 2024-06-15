07cc109febcf99cf943b9f73d74e79017ece48c0
José Ricardo <josericardosaraiva7@gmail.com>
2024-06-15 17:43:25 -0300

    get user in signIn

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,9 +1,10 @@
 import { ReactNode, createContext, useState } from "react";
 import { UserDTO } from "@dtos/UserDTO";
+import { api } from "@services/api";
 
 export type AuthContextDataProps = {
     user: UserDTO;
-    signIn: (email: string, password: string) => void;
+    signIn: (email: string, password: string) => Promise<void>;
 }
 
 type AuthContextProviderProps = {
@@ -12,21 +13,22 @@ type AuthContextProviderProps = {
 
 export const AuthContext = createContext<AuthContextDataProps>({} as AuthContextDataProps);
 
-export function AuthContextProvider({ children }: AuthContextProviderProps) {
-    const [user, setUser] = useState({
-        id: '1',
-        name: 'José Ricardo',
-        email: 'catce.2020111tads0360@aluno.ifpi.edu.br',
-        avatar: 'jose.png'
-    });
 
-    function signIn(email: string, password: string) {
-        setUser({
-            id: '',
-            name: '',
-            email,
-            avatar: '',
-        });
+export function AuthContextProvider({ children }: AuthContextProviderProps) {
+    const [user, setUser] = useState<UserDTO>({} as UserDTO);
+    
+    async function signIn(email: string, password: string) {
+        try {
+
+            const { data } = await api.post('/sessions', {email, password});
+        
+            if(data.user) {
+                setUser(data.user);
+            }
+
+        } catch (error) {
+            throw error;
+        } 
     }
 
     return(
--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,9 +1,11 @@
 import { Controller, useForm } from "react-hook-form";
 import { useNavigation } from "@react-navigation/native";
-import { VStack, Image, Text, Center, Heading, ScrollView } from "native-base"; 
+import { VStack, Image, Text, Center, Heading, ScrollView, useToast } from "native-base"; 
 
 import { AuthNavigatorRoutesProps } from "@routes/auth.routes";
 import { useAuth } from "@hooks/useAuth";
+
+import { AppError } from "@utils/AppError";
 
 import LogoSvg from '@assets/logo.svg';
 import BackgroundImg from '@assets/background.png';
@@ -19,6 +21,8 @@ type FormData = {
 export function SignIn() {
     const { signIn } = useAuth();
 
+    const toast = useToast();
+
     const navigation = useNavigation<AuthNavigatorRoutesProps>();
 
     const { control, handleSubmit, formState: { errors }} = useForm<FormData>();
@@ -27,8 +31,22 @@ export function SignIn() {
         navigation.navigate('signUp');
     }
 
-    function handleSignIn({ email, password }: FormData) {
-        signIn(email, password);
+    async function handleSignIn({ email, password }: FormData) {
+        try {
+
+            await signIn(email, password);
+
+        } catch (error) {
+            const isAppError = error instanceof AppError;
+
+            const title = isAppError ? error.message : 'Não foi possível entrar. Tente novamente mais tarde.';
+
+            toast.show({
+                title,
+                placement: 'top',
+                bgColor: 'red.500'
+            })
+        }
     }
 
     return(

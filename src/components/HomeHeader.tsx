6e79531ec547ae13ca12e9600d2a336558ec9dc6
José Ricardo <josericardosaraiva7@gmail.com>
2024-06-16 10:03:02 -0300

    add user datas in home header

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,25 +1,31 @@
 import { TouchableOpacity } from "react-native";
 import { HStack, Heading, VStack, Text, Icon } from "native-base";
-
 import { MaterialIcons } from "@expo/vector-icons";
 
 import { UserPhoto } from "./UserPhoto";
+import defaultUserPhotoImg from "@assets/userPhotoDefault.png";
+
+import { useAuth } from "@hooks/useAuth";
 
 
 export function HomeHeader() {
+    const { user } = useAuth();
+
     return (
         <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
             <UserPhoto 
-                size={16}
-                source={{uri: 'https://avatars.githubusercontent.com/u/61853617?v=4'}}
+                source={user.avatar ? {uri: user.avatar}: defaultUserPhotoImg}
                 alt="user"
+                size={16}
                 mr={4}
             />
 
             <VStack flex={1}>
                 <Text color="gray.100" fontSize="md">Olá</Text>
 
-                <Heading color="gray.100" fontSize="md" fontFamily="heading">José Ricardo</Heading>
+                <Heading color="gray.100" fontSize="md" fontFamily="heading">
+                    {user.name}
+                </Heading>
             </VStack>
 
             <TouchableOpacity>

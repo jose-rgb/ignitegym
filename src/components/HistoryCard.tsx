238971e0f0a1b267c72d003c0e93f59c43b020d9
José Ricardo <josericardosaraiva7@gmail.com>
2024-06-19 18:10:45 -0300

    fetch history data

--- src/components/HistoryCard.tsx
+++ src/components/HistoryCard.tsx
@@ -1,6 +1,11 @@
+import { HistoryDTO } from "@dtos/HistoryDTO";
 import { HStack, Heading, VStack, Text } from "native-base";
 
-export function HistoryCard(){
+type Props = {
+    data: HistoryDTO;
+  }
+
+export function HistoryCard({data}: Props){
     return(
         <HStack 
             w="full" 
@@ -13,11 +18,11 @@ export function HistoryCard(){
             justifyContent="space-between"
         >
             <VStack mr={5} flex={1}>
-                <Heading color="white" fontSize="md" textTransform="capitalize" numberOfLines={1} fontFamily="heading"> Costas </Heading>
-                <Text color="gray.100" fontSize="lg" numberOfLines={1}> Puxada frontal</Text>
+                <Heading color="white" fontSize="md" textTransform="capitalize" numberOfLines={1} fontFamily="heading"> {data.group} </Heading>
+                <Text color="gray.100" fontSize="lg" numberOfLines={1}> {data.name} </Text>
             </VStack>
 
-            <Text color="gray.300" fontSize="md"> 08:56 </Text>
+            <Text color="gray.300" fontSize="md"> {data.hour} </Text>
         </HStack>
     );
 }

b6f64c2306c00ba695c4467ecbd48520851e75ef
José Ricardo <josericardosaraiva7@gmail.com>
2024-06-20 17:05:21 -0300

    password confirm validation

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -28,8 +28,19 @@ type FormDataProps = {
 
 const profileSchema = yup.object({
     name: yup.string().required('Informe o nome'),
-    password: yup.string().min(6, 'A senha deve ter pelo menos 6 dígitos!').nullable().transform((value) => !!value ? value : null),
-    confirm_password: yup.string().nullable().transform((value) => !!value ? value : null).oneOf([yup.ref('password')], 'Senha não confere!')
+    password: yup
+    .string()
+    .min(6, 'A senha deve ter pelo menos 6 dígitos!')
+    .nullable().transform((value) => !!value ? value : null),
+    confirm_password: yup
+    .string()
+    .nullable()
+    .transform((value) => !!value ? value : null)
+    .oneOf([yup.ref('password')], 'Senha não confere!')
+    .when('password', {
+        is: (Field: any) => Field,
+        then: (schema) => schema.nullable().required('Informe a confirmação da senha!').transform((value) => !!value ? value : null)
+    })
 })
 
 export function Profile(){
